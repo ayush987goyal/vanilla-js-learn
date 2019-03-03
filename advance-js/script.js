a3fd41c38099d76d18ce434cf8d61412768d8b26
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-03 22:44:08 +0530

    Object create

--- advance-js/script.js
+++ advance-js/script.js
@@ -1,5 +1,5 @@
 // Function construtor
-
+/*
 var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
@@ -20,3 +20,24 @@ jane.calculateAge();
 mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
+*/
+
+// Object.Create
+/*
+var personProto = {
+  calculateAge: function() {
+    console.log(2019 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+  name: { value: 'Jane' },
+  yearOfBirth: { value: 1969 },
+  job: { value: 'designer' }
+});
+*/
