0f35f406a38ee41083d162da58a041e281750f9d
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-03 23:33:34 +0530

    Adding closures

--- advance-js/script.js
+++ advance-js/script.js
@@ -125,3 +125,40 @@ designerQuestion('Jane');
 */
 
 /************************************************************************************************ */
+
+// Closures
+/*
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+
+  return function(yearOfBirth) {
+    var age = 2019 - yearOfBirth;
+    console.log(retirementAge - age + a);
+  };
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+
+retirement(66)(1990);
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+      console.log('What subject do you teach ' + name + '?');
+    } else {
+      console.log('Hello! ' + name);
+    }
+  };
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('Jane');
+*/
+
+/************************************************************************************************ */
