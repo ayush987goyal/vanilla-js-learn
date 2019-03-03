1977e83a31efc9ce0b6b3c1d8538c06ed12ae3c9
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-03 19:37:59 +0530

    Add roll dice listener

--- dom-mani-events/app.js
+++ dom-mani-events/app.js
@@ -9,18 +9,23 @@ GAME RULES:
 
 */
 
-var score, roundScore, activePlayer, dice;
+var score, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+});
