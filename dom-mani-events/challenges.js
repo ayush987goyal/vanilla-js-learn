9747c9dc70acf1806c5ab96fda939d065c9a3221
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-03 21:51:38 +0530

    Add another dice

--- dom-mani-events/challenges.js
+++ dom-mani-events/challenges.js
@@ -16,12 +16,21 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if (!gamePlaying) return;
 
-  var dice = Math.floor(Math.random() * 6) + 1;
+  var dice1 = Math.floor(Math.random() * 6) + 1;
+  var dice2 = Math.floor(Math.random() * 6) + 1;
 
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
+  document.getElementById('dice-1').style.display = 'block';
+  document.getElementById('dice-2').style.display = 'block';
+  document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+  document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
+  if (dice1 !== 1 && dice2 !== 1) {
+    roundScore += dice1 + dice2;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    nextPlayer();
+  }
+  /*
   if (dice === 6 && lastDice === 6) {
     scores[activePlayer] = 0;
     roundScore = 0;
@@ -36,6 +45,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   }
 
   lastDice = dice;
+  */
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
@@ -52,7 +62,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     gamePlaying = false;
 
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
     document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
     document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
   } else {
@@ -70,7 +81,8 @@ function nextPlayer() {
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -81,7 +93,8 @@ function init() {
   activePlayer = 0;
   gamePlaying = true;
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
