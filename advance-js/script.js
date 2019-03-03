7d5145f66ecd899ed09c23deb6caa87dac2ec82e
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-03 22:58:02 +0530

    Pass func as args

--- advance-js/script.js
+++ advance-js/script.js
@@ -41,3 +41,29 @@ var jane = Object.create(personProto, {
   job: { value: 'designer' }
 });
 */
+
+// Functions
+
+// Passing func as arguments
+var years = [1990, 1965, 1937, 2011, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calcAge(el) {
+  return 2019 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+var ages = arrayCalc(years, calcAge);
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(ages);
+console.log(fullAges);
