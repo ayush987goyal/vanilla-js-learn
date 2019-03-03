b6a60342f893ba3c97b0ac40e4ab6a809d5c940f
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-03 20:50:34 +0530

    Add new game func

--- dom-mani-events/app.js
+++ dom-mani-events/app.js
@@ -11,16 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   var dice = Math.floor(Math.random() * 6) + 1;
@@ -64,4 +55,26 @@ function nextPlayer() {
   document.querySelector('.player-1-panel').classList.toggle('active');
 
   document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector(`.player-0-panel`).classList.remove('winner');
+  document.querySelector(`.player-1-panel`).classList.remove('winner');
+  document.querySelector(`.player-0-panel`).classList.remove('active');
+  document.querySelector(`.player-1-panel`).classList.remove('active');
+  document.querySelector(`.player-0-panel`).classList.add('active');
 }
