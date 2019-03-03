d504806eeb98e9283a122b6d0ff0297593fc1ebe
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-03 22:33:46 +0530

    Add Person with prototype construcotr

---
+++ advance-js/script.js
@@ -0,0 +1,22 @@
+// Function construtor
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person.prototype.calculateAge = function() {
+  console.log(2019 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+console.log(john.lastName);
+console.log(jane.lastName);
