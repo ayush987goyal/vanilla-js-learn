b7ae57d04ae6e4727cce92735b7d2ce5d3503040
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-04 10:06:27 +0530

    Add initial listeners for add

--- budget-app/app.js
+++ budget-app/app.js
@@ -1,27 +1,22 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
+  // some code
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
   // some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
+  var ctrlAddItem = function() {};
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
     }
-  };
+  });
 })(budgetController, UIController);
