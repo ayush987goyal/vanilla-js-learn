fba0c062830906cb5bf9f6f792dc652ed70cc574
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-04 20:18:57 +0530

    Add class inheritance

--- es6-nex/script.js
+++ es6-nex/script.js
@@ -1,5 +1,6 @@
 // Classes
 
+/*
 // ES5
 var Person5 = function(name, yearOfBirth, job) {
   this.name = name;
@@ -34,3 +35,63 @@ class Person6 {
 
 const john6 = new Person6('John', 1990, 'teacher');
 Person6.greeting();
+*/
+
+// Classes and subclasses
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calcAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+};
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job);
+  this.olympicGames = olympicGames;
+  this.medals = medals;
+};
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals);
+};
+
+var john5 = new Person5('John', 1990, 'swimmer');
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+// ES6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calcAge() {
+    const age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+}
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
+
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals);
+  }
+}
+
+const john6 = new Person6('John', 1990, 'teacher');
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
