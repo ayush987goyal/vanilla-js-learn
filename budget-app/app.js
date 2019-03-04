23547a1fbb79354fb31edb1fd4e05822fc3ef059
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-04 10:18:58 +0530

    Add way to get input values

--- budget-app/app.js
+++ budget-app/app.js
@@ -5,14 +5,38 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-  // some code
+  var domStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(domStrings.inputType).value, // will be inc or exp
+        description: document.querySelector(domStrings.inputDescription).value,
+        value: document.querySelector(domStrings.inputValue).value
+      };
+    },
+
+    getDomStrings: function() {
+      return domStrings;
+    }
+  };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-  var ctrlAddItem = function() {};
+  var domStrings = UICtrl.getDomStrings();
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  var ctrlAddItem = function() {
+    var input = UICtrl.getInput();
+    console.log(input);
+  };
+
+  document.querySelector(domStrings.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
