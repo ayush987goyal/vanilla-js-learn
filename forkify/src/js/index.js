66f00a15bcc3310f889bfb5941e593a3ea0e55db
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-05 08:16:33 +0530

    Add basic axios call to api

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,1 +1,12 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+  try {
+    const key = 'a06658be68a261203d9759c6b5db0661';
+    // const results = await axios.get(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    // console.log(results.data.recipes);
+  } catch (err) {
+    alert(err);
+  }
+}
+getResults('pizza');
