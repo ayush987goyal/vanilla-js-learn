3b975bcbaf7315e5f4c4bf1086c6629566168fde
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-05 08:29:30 +0530

    Add search controller

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,3 +1,28 @@
 import Search from './models/Search';
 
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping List object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  const query = 'pizza'; // TODO
+
+  if (query) {
+    state.search = new Search(query);
+
+    await state.search.getResults();
+
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
 const search = new Search('pizza');
