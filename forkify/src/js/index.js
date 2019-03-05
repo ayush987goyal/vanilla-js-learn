43c2515a1f804ffad4e27a80ed6107960a479290
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-05 22:25:10 +0530

    update servings

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -80,3 +80,15 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+});
