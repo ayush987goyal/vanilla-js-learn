8fdff8b9bc7c29949e67a449803f937dd9d46b4b
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-05 09:11:48 +0530

    Add loader

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -18,8 +18,11 @@ const controlSearch = async () => {
 
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     await state.search.getResults();
+
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
--- forkify/src/js/views/searchView.js
+++ forkify/src/js/views/searchView.js
@@ -10,6 +10,22 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+  if (title.length > limit) {
+    const newTitle = [];
+
+    title.split(' ').reduce((acc, word) => {
+      if (acc + word.length <= limit) {
+        newTitle.push(word);
+      }
+      return acc + word.length;
+    }, 0);
+
+    return `${newTitle.join(' ')} ...`;
+  }
+  return title;
+};
+
 const renderRecipe = recipe => {
   const markup = `
     <li>
@@ -18,7 +34,7 @@ const renderRecipe = recipe => {
               <img src="${recipe.image_url}" alt="${recipe.title}">
           </figure>
           <div class="results__data">
-              <h4 class="results__name">${recipe.title}</h4>
+              <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
               <p class="results__author">${recipe.publisher}</p>
           </div>
       </a>
